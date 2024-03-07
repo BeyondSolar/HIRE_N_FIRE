508d4845a761ecfd4cb23efd9bca71b422cda9c8
TechnoWanders <mrtechonipo@gmail.com>
2024-03-07 22:40:47 +0530

    First Work

---
+++ frontend/src/components/Banner.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <img src="./images/banner.png" alt="Description of the image" />
+    </div>
+  );
+}
+
+export default Banner;
--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,9 +1,13 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
 
 const Layout = ()=>{
     return (
         <div className="flex flex-col min-h-screen">
             <Header/>
+            <Banner/>
+            <Footer/>
         </div>
     )
 }
