50ca24a4edd097ce40ded38feafc8557dacca2ef
BeyondSolar <abhicsjuet@gmail.com>
2024-04-02 21:00:24 +0530

    Cleanup

--- frontend/src/components/Banner.tsx
+++ frontend/src/components/Banner.tsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
 const Banner = () => {
   return (
     <div className="banner">
--- frontend/src/components/Photo2.tsx
+++ frontend/src/components/Photo2.tsx
@@ -1,7 +1,3 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import{FaMapPin} from 'react-icons/fa';
-
 const Photo2 = () => {
   return (
     <div className="photo2">
