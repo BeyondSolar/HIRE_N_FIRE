30734743171eacc63a9d14135a578a1342bff6db
TechnoWanders <mrtechonipo@gmail.com>
2024-04-07 21:06:29 +0530

    FIND TEAMS SECTION WORK STARTED SEARCH BAR LOADING

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,15 @@ import {
 
 import Layout from "./layouts/Layout";
 import Login from "./layouts/Login"
+import FindTeams from "/home/naveen/Documents/FREQUENT/STUDY RELATED STUFFS/CODES/6th semester shit/HIRE N FIRE/HIRE_N_FIRE/frontend/src/layouts/FindTeams.tsx"
 
 const App = ()=>{
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Layout></Layout>} />
-        <Route path='/login' element={<Login></Login>} />
+        <Route path='/' element={<Layout></Layout>}/>
+        <Route path='/login' element={<Login></Login>}/>
+        <Route path='/findteams' element={<FindTeams></FindTeams>}/>
       </Routes>
     </Router>
   )
