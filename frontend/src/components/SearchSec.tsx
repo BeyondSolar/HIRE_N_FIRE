dcc6ce2dad8dcb8724bbf95e1995f88b694e394e
TechnoWanders <mrtechonipo@gmail.com>
2024-04-08 19:57:19 +0530

    STUCK IN FIND TALENTS PAGE

--- frontend/src/components/SearchSec.tsx
+++ frontend/src/components/SearchSec.tsx
@@ -1,12 +1,29 @@
 const SearchSec = ()=> {
         return(
+        <div className="full">
             <div className="section1">
-                <p>Search Section</p>
+                <p>Search Fields</p>
                 <div className="searching1">
                     <input type="text" id="search-input" placeholder="Search..."/>
-                    <ul className="suggestions" id ="suggestions-list"></ul>
+                    <button type="submit" className="search-button"/>
+                </div>
+
+                <div className="box1">
+                </div>
+
+                <div className="box2">
+                </div>
+
+            </div>
+            
+            <div className="section2">
+                <div className="cards">
+                    <div id ="root"></div>
                 </div>
             </div>
+        </div>
+            
+
         );
 }
 
