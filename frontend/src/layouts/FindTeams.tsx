31cddc62013c3b96e26c30899d63d6f886f6e27b
BeyondSolar <abhicsjuet@gmail.com>
2024-04-08 18:23:34 +0530

    Some changes

--- frontend/src/layouts/FindTeams.tsx
+++ frontend/src/layouts/FindTeams.tsx
@@ -1,5 +1,5 @@
 import SearchSec from "../components/SearchSec";
-import Header from "/home/naveen/Documents/FREQUENT/STUDY RELATED STUFFS/CODES/6th semester shit/HIRE N FIRE/HIRE_N_FIRE/frontend/src/components/Header.tsx";
+import Header from "../components/Header.tsx";
 
 const FindTeams = ()=>{
     return(
--- frontend/src/layouts/Login.tsx
+++ frontend/src/layouts/Login.tsx
@@ -1,5 +1,5 @@
 import LoginBox from "../components/LoginBox";
-import '/home/naveen/Documents/FREQUENT/STUDY RELATED STUFFS/CODES/6th semester shit/HIRE N FIRE/HIRE_N_FIRE/frontend/src/assets/css/Login.css';
+import '../assets/css/Login.css';
 
 const Login = ()=>{
     return (
